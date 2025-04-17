914acf2b7bf072b050e3a93a2f982d9480ed3ff2
mariabarbalho <barbalho.m@aluno.ifsp.edu.br>
2025-04-16 22:20:46 -0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baze;
+            int altura;
+            int area;
+
+            Console.Write("Informe o valor da base do retângulo: ");
+            baze = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o valor do retângulo:");
+            altura = int.Parse(Console.ReadLine()); 
+
+            area = baze * altura;
+
+            Console.WriteLine("O valor da área e um retângulo que tem base de {0} e altura de {1} é [2}", baze, altura, area);
+        }
+    }
+}
